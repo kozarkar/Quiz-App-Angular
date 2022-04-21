134f5938cc996c0b415c49c452ecdac4be222650
Kshitij Ozarkar <ksozarkar@outlook.com>
2022-04-21 18:33:29 +0530

    result page added

--- src/app/mcq-question/mcq-question.component.ts
+++ src/app/mcq-question/mcq-question.component.ts
@@ -15,10 +15,11 @@ export class McqQuestionComponent implements OnInit {
   counter=60;
   correctAnswer: number = 0;
   inCorrectAnswer: number =0;
+  
   interval$ : any;
   selection: any = [];
   op: any ;
-
+  progress: string ="0";
   stopTimer : any;
   time = 0;
   dt = new Date(new Date().setTime(0));
@@ -27,6 +28,8 @@ export class McqQuestionComponent implements OnInit {
   mintes = Math.floor((this.ctime % (1000*60*60))/(1000*60));
   formated_sec : any = "00";
   formated_min : any = "00";
+  quizShow: boolean = true;
+  resultShow: boolean = false;
 
   constructor(private questionservice: QuestionService) { }
 
@@ -44,10 +47,12 @@ export class McqQuestionComponent implements OnInit {
 
     nextQuestion(){
       this.currentQuestion++;
+      this.getProgressPercent();
       if(this.selection[this.currentQuestion] != null){
         this.op = this.selection[this.currentQuestion]
-
+        
       }else{
+        
         this.op = null;
       }
     }
@@ -55,20 +60,36 @@ export class McqQuestionComponent implements OnInit {
     previousQuestion(){
       this.currentQuestion--;
       this.op = this.selection[this.currentQuestion];
+      this.getProgressPercent();
     }
 
-    answer(currentQno: number, i:any){
-      this.selection[currentQno-1]= i;       
-    }
-
-    postAnswer(id:any, data:any){
-      this.questionservice.postMCQanswers(id,data)
-      .subscribe((res=>{       
-      }))     
-    }
+    answer(currentQno: number, i:any, option:any){
+      this.selection[currentQno-1]= i;
     
+      if(option.correct){
+        this.correctAnswer++;
+        this.getProgressPercent();
+
+        // let options = document.querySelectorAll(".answers-indicator div");
+        // options[this.currentQuestion].classList.add("correct");
+
+      }else{
+        this.inCorrectAnswer++;
+        console.log("incorrect");
+        this.getProgressPercent();
+
+        // let options = document.querySelectorAll(".answers-indicator div");
+        // options[this.currentQuestion].classList.add("incorrect");
+
+
+      }
+      let indicator = document.querySelectorAll(".answers-indicator div");
+      indicator[currentQno].classList.add("fill");
+    }
+
     timer(){
       this.stopTimer = setInterval(()=>{
+        if(this.quizShow == true){
         this.time++;
         if(this.seconds < 59){
           this.seconds++;
@@ -76,25 +97,19 @@ export class McqQuestionComponent implements OnInit {
           this.seconds = 0;
           this.mintes++;
         }
+      }
         this.formated_sec = this.seconds < 10 ? `0${this.seconds}` : `${this.seconds}`;
         this.formated_min = this.mintes < 10 ? `0${this.mintes}` : `${this.mintes}`;
       },1000)
     }
-
-    startCounter(){
-      this.interval$ = interval(1000)
-      .subscribe(val=>{
-        this.counter--;
-        if(this.counter===0){
-          this.currentQuestion++;
-          this.counter=60;
-        }
-      })
+    getProgressPercent(){
+      this.progress = ((this.currentQuestion/(this.mcqQuestionList.length-1))*100).toString();
+      return this.progress;
     }
-    stopCounter(){
 
-    }
-    resetCounter(){
-
+    showResults(){
+        this.resultShow = true;
+        this.quizShow = false;
     }
 }
+
